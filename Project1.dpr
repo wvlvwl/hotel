a42aec89bd7abe1b940d44744d10d52abe3458fe
wvlvwl <37897884+wvlvwl@users.noreply.github.com>
2018-12-26 00:10:20 +0300

    Add files via upload

---
+++ Project1.dpr
@@ -0,0 +1,32 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  Unit2 in 'Unit2.pas' {Form2},
+  Unit10 in 'Unit10.pas' {Form10},
+  Unit3 in 'Unit3.pas' {Form3},
+  Unit4 in 'Unit4.pas' {Form4},
+  Unit5 in 'Unit5.pas' {Form5},
+  Unit6 in 'Unit6.pas' {Form6},
+  Unit7 in 'Unit7.pas' {Form7},
+  Unit8 in 'Unit8.pas' {Form8};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm10, Form10);
+  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm2, Form2);
+  Application.CreateForm(TForm10, Form10);
+  Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TForm4, Form4);
+  Application.CreateForm(TForm4, Form4);
+  Application.CreateForm(TForm5, Form5);
+  Application.CreateForm(TForm6, Form6);
+  Application.CreateForm(TForm7, Form7);
+  Application.CreateForm(TForm8, Form8);
+  Application.Run;
+end.
